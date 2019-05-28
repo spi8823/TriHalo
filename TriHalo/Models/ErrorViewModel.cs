5b48b8d4e492fe4e8832b9627fd72d8ea1cb7bee
Shintaro <shin.8823@outlook.jp>
2019-05-28 23:36:27 +0900

    ASP.NET CoreのMVCテンプレートでソリューション作成

---
+++ TriHalo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TriHalo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
