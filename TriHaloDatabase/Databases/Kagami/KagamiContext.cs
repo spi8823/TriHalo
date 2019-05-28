b64d9d5904ef877710cc6a4d74845bb2284b458e
Shintaro <shin.8823@outlook.jp>
2019-05-29 02:03:40 +0900

    DbContextを継承したKagamiContextと、Jinjaクラスの追加およびテストコード

---
+++ TriHaloDatabase/Databases/Kagami/KagamiContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace TriHaloDatabase.Databases.Kagami
+{
+    class KagamiContext : DbContext
+    {
+        public DbSet<Jinja> Jinjas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connection = @"server=localhost;database=Kagura;userid=root;pwd=root;sslmode=none;";
+            optionsBuilder.UseMySQL(connection);
+        }
+    }
+}
--- TriHaloDatabase/Program.cs
+++ TriHaloDatabase/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TriHaloDatabase.Databases.Kagami;
 
 namespace TriHaloDatabase
 {
@@ -7,6 +11,20 @@ namespace TriHaloDatabase
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            using (var context = new KagamiContext())
+            {
+                context.Jinjas.Add(new Jinja { ID = 0, Name = "test" });
+                context.SaveChanges();
+            }
+
+            using (var context = new KagamiContext())
+            {
+                foreach(var jinja in context.Jinjas)
+                {
+                    Console.WriteLine(jinja.Name);
+                }
+            }
         }
     }
 }
