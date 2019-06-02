8adb7e0081108bbdb110da248cfb0902dc37dc53
Shintaro <shin.8823@outlook.jp>
2019-06-02 21:57:05 +0900

    データベース用プロジェクト作成

---
+++ TriHaloDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriHaloDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
