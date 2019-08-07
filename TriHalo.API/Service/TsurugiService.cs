f4bbe8c4ce94bd7bf661bd8e5d596e41d9f33fc7
Shintaro <shin.8823@outlook.jp>
2019-08-07 09:22:50 +0900

    サービス

--- TriHalo.API/Service/TsurugiService.cs
+++ TriHalo.API/Service/TsurugiService.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using TriHalo.Common.Tsurugi;
+//using Newtonsoft.Json;
 
 namespace TriHalo.API.Service
 {
     public class TsurugiService
     {
-        public const string BaseURL = "/api/Tsurugi/";
+        public const string BaseURL = "/api/Tsurugi";
+        private HttpClient Client { get; } = new HttpClient();
+
+        public async Task<int> PostBook(Book book)
+        {
+            var url = $"{BaseURL}/";
+            return 1;
+        }
     }
 }
