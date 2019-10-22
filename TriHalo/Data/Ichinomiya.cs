f9793a6337f1854f85931696aa836757deafa0c8
Shintaro <shin.8823@outlook.jp>
2019-10-23 02:07:27 +0900

    資料

---
+++ TriHalo/Data/Ichinomiya.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TriHalo.Data
+{
+    public class Ichinomiya
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ TriHalo/Data/MyService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TriHalo.Data
+{
+    public class MyService
+    {
+        private MyContext _Context { get; }
+
+        public MyService(MyContext context)
+        {
+            _Context = context;
+        }
+
+        public async Task ClearIchinomiyas()
+        {
+            _Context.Ichinomiyas.RemoveRange(await GetIchinomiyas());
+            _Context.SaveChanges();
+        }
+
+        public async Task<List<Ichinomiya>> GetIchinomiyas()
+        {
+            return await _Context.Ichinomiyas.ToListAsync();
+        }
+
+        public async Task AddIchinomiya(Ichinomiya ichinomiya)
+        {
+            await _Context.Ichinomiyas.AddAsync(ichinomiya);
+            await _Context.SaveChangesAsync();
+        }
+    }
+
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ichinomiya> Ichinomiyas { get; set; }
+    }
+}
